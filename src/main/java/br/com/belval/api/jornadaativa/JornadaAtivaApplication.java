99c603f817ee42a7be24835d46f7b8c8740c39f3
Erik <erik15casa@gmail.com>
2025-04-30 21:59:13 -0300

    Versão inicial do projeto gestao de corrida

---
+++ src/main/java/br/com/belval/api/jornadaativa/JornadaAtivaApplication.java
@@ -0,0 +1,13 @@
+package br.com.belval.api.jornadaativa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JornadaAtivaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JornadaAtivaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/belval/api/jornadaativa/JornadaAtivaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.belval.api.jornadaativa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JornadaAtivaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
